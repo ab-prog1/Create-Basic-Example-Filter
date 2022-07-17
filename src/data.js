b6a8732e2f80ad69a35dfa813419f5a848667908
Abolfazl Ghorbani <ab.ghorbani28@gmail.com>
2022-07-17 14:43:41 +0200

    Create-Basic-Example-Filter.git

---
+++ src/data.js
@@ -0,0 +1,60 @@
+const menu = [
+    {
+        id: 1,
+        title: 'Laptop',
+        category: 'Laptop',
+        price: 12000000,
+        img: './images/1.jpg',
+        desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+    },
+    {
+        id: 2,
+        title: 'Headphone',
+        category: 'Headphone',
+        price: 12000000,
+        img: './images/2.jpg',
+        desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. `
+    },
+    {
+        id: 3,
+        title: 'Laptop',
+        category: 'Laptop',
+        price: 12000000,
+        img: './images/3.jpg',
+        desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. `
+    },
+    {
+        id: 4,
+        title: 'Mobile',
+        category: 'Mobile',
+        price: 12000000,
+        img: './images/4.jpg',
+        desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+    },
+    {
+        id: 5,
+        title: 'Headphone',
+        category: 'Headphone',
+        price: 12000000,
+        img: './images/5.jpg',
+        desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+    },
+    {
+        id: 6,
+        title: 'Laptop',
+        category: 'Laptop',
+        price: 12000000,
+        img: './images/6.jpg',
+        desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+    },
+    {
+        id: 7,
+        title: 'Mobile',
+        category: 'Mobile',
+        price: 12000000,
+        img: './images/7.jpg',
+        desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+    },
+]
+
+export default menu
